e7e1398e032512ea2e582649a761b97f6a442f26
Hugh <intelzombi@users.noreply.github.com>
2019-10-24 09:04:18 -0500

    modularize Method Channel calls to channels package to clean up classes continued

--- lib/screens/create_password.dart
+++ lib/screens/create_password.dart
@@ -21,26 +21,16 @@ class _CreatePasswordState extends State<CreatePassword> {
   TextEditingController passwordController = TextEditingController();
 
   // local state variables
-  String _initialWelcomeMessage = "Welcome to Bone Dry Password Wallet. "
-      "The first step is to create a password.  This will be the only password you have to remember."
-      "Make is Strong.  No Spaces.  Recommendations include a minimum of 10 characters, at least one of each of the following:"
-      "an uppercase character, a number, and a special character. "
-      "Heeding the recommendation is up to you.  Your password is yours to choose.  "
-      "Disclaimer;  This software is not meant as a sole repository for passwords.  Your password is never saved anywhere in this application. "
-      "If you forget you password there is no "
-      "ability to recover your wallet items.  They will be lost.  Always keep a copy of your passwords in a safe secure place.  "
-      "This software is a convienience application only.  It is meant to be able to give you an easy way to store and retrieve all of "
-      "your passwords and only having to remember one password.  The one you are about to choose.  It bears repeating; If you forget "
-      "your password there is no way to retrieve the data.  Back your passwords up.  This application should not be used as your only "
-      "way to remeber your passwords. Use the software at your own risk";
-
   String _welcomeMessage =
-      "Welcome to Bone Dry Password Wallet. First Up you must create a Password";
+      "Welcome to Bone Dry Password Wallet. First up you must create a Password. It is important to note that the password you " +
+      "are choosing is not saved anywhere with this application. If you forget this password there is no way to recover the " +
+      "password or any items stored " +
+      "in this wallet. This password will be needed to access the items in this wallet. This tool is not meant to be used as a kype " +
+      "for your passwords. It is only a tool of convenience.";
+
   String _acknowledgementMessage = "CreatePassword Successful";
   String _updateAcknowledgementMessage = "Password Update Successful";
   String _password = "";
-
-  bool _welcomeAcknowledged = false;
   DatabaseHelper databaseHelper = DatabaseHelper();
   List<SaltPepperItem> _saltPepperItemList;
   int spCount = 0;
@@ -78,9 +68,7 @@ class _CreatePasswordState extends State<CreatePassword> {
                 Padding(
                   padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
                   child: Text(
-                    _welcomeAcknowledged
-                        ? _initialWelcomeMessage
-                        : _welcomeMessage,
+                    _welcomeMessage,
                     textScaleFactor: .85,
                     style: textStyle,
                     softWrap: true,
@@ -119,14 +107,14 @@ class _CreatePasswordState extends State<CreatePassword> {
   void _getSaltPepper() async {
     final Future<Database> dbFuture = databaseHelper.initializeDatabase();
     dbFuture.then((database) {
-      Future<int> count = _getSaltPepperCount(context);
+      Future<int> count = _getSaltPepperCount();
       count.then((cnt) {
         if (cnt == 0) {
           Future<Uint8List> futureSalt = CypherChannel.generateSalt(saltSetState);
           futureSalt.then((salt) {
             Future<Uint8List> futurePepper = CypherChannel.generatePepper(pepperSetState);
             futurePepper.then((pepper) {
-              _insertSaltPepper(context,
+              _insertSaltPepper(
                   new SaltPepperItem(SaltPepper.salt, SaltPepper.pepper));
             });
           });
@@ -150,14 +138,14 @@ class _CreatePasswordState extends State<CreatePassword> {
   void _generateSaltPepper() async {
     final Future<Database> dbFuture = databaseHelper.initializeDatabase();
     dbFuture.then((database) {
-      Future<int> count = _getSaltPepperCount(context);
+      Future<int> count = _getSaltPepperCount();
       count.then((cnt) {
         if (cnt == 0) {
           Future<Uint8List> futureSalt = CypherChannel.generateSalt(saltSetState);
           futureSalt.then((salt) {
             Future<Uint8List> futurePepper = CypherChannel.generatePepper(pepperSetState);
             futurePepper.then((pepper) {
-              _insertSaltPepper(context,
+              _insertSaltPepper(
                   new SaltPepperItem(SaltPepper.salt, SaltPepper.pepper));
             });
           });
@@ -166,7 +154,7 @@ class _CreatePasswordState extends State<CreatePassword> {
           futureSalt.then((salt) {
             Future<Uint8List> futurePepper = CypherChannel.generatePepper(pepperSetState);
             futurePepper.then((pepper) {
-              _updateSaltPepper(context,
+              _updateSaltPepper(
                   new SaltPepperItem(SaltPepper.salt, SaltPepper.pepper));
             });
           });
@@ -175,25 +163,15 @@ class _CreatePasswordState extends State<CreatePassword> {
     });
   }
 
-  void _showAlertDialog(String lockerName, String message) {
-    AlertDialog alertDialog = AlertDialog(
-      title: Text(lockerName),
-      content: Text(message),
-    );
-    showDialog(context: context, builder: (_) => alertDialog);
-  }
-
-  void _insertSaltPepper(
-      BuildContext context, SaltPepperItem saltPepperItem) async {
+  void _insertSaltPepper(SaltPepperItem saltPepperItem) async {
     int result = await databaseHelper.insertSaltPepperItem(saltPepperItem);
   }
 
-  void _updateSaltPepper(
-      BuildContext context, SaltPepperItem saltPepperItem) async {
+  void _updateSaltPepper(SaltPepperItem saltPepperItem) async {
     int result = await databaseHelper.updateSaltPepperItem(saltPepperItem);
   }
 
-  Future<int> _getSaltPepperCount(BuildContext context) async {
+  Future<int> _getSaltPepperCount() async {
     int result = await databaseHelper.getSaltPepperCount();
     return result;
   }
